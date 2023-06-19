b193d3e1bc90941663c9666f99ac0d3f6fea884e
Rasheeq Kamal <41065855+pizzaandpineapples@users.noreply.github.com>
2023-06-19 18:27:43 +0600

    Completed video 6 from the playlist

--- EmployeeManagement/Startup.cs
+++ EmployeeManagement/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -32,7 +33,8 @@ namespace EmployeeManagement
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response
+                        .WriteAsync(System.Diagnostics.Process.GetCurrentProcess().ProcessName);
                 });
             });
         }
